cf2aae3357a7ccb4f58945cd1f2be788d3814e60
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-08-30 11:20:45 -0700

    finish the token

---
+++ contracts/SealHubMemorabiliaToken.sol
@@ -0,0 +1,101 @@
+//                                                                        ,-,
+//                            *                      .                   /.(              .
+//                                       \|/                             \ {
+//    .                 _    .  ,   .    -*-       .                      `-`
+//     ,'-.         *  / \_ *  / \_      /|\         *   /\'__        *.                 *
+//    (____".         /    \  /    \,     __      .    _/  /  \  * .               .
+//               .   /\/\  /\/ :' __ \_  /  \       _^/  ^/    `—./\    /\   .
+//   *       _      /    \/  \  _/  \-‘\/  ` \ /\  /.' ^_   \_   .’\\  /_/\           ,'-.
+//          /_\   /\  .-   `. \/     \ /.     /  \ ;.  _/ \ -. `_/   \/.   \   _     (____".    *
+//     .   /   \ /  `-.__ ^   / .-'.--\      -    \/  _ `--./ .-'  `-/.     \ / \             .
+//        /     /.       `.  / /       `.   /   `  .-'      '-._ `._         /.  \
+// ~._,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'2_,-'
+// ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~ ~~~~~~~~
+// ~~    ~~~~    ~~~~     ~~~~   ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~~~    ~~
+//     ~~     ~~      ~~      ~~      ~~      ~~      ~~      ~~       ~~     ~~      ~~      ~~
+//                          ๐
+//                                                                              _
+//                                                  ₒ                         ><_>
+//                                  _______     __      _______
+//          .-'                    |   _  "\   |" \    /" _   "|                               ๐
+//     '--./ /     _.---.          (. |_)  :)  ||  |  (: ( \___)
+//     '-,  (__..-`       \        |:     \/   |:  |   \/ \
+//        \          .     |       (|  _  \\   |.  |   //  \ ___
+//         `,.__.   ,__.--/        |: |_)  :)  |\  |   (:   _(  _|
+//           '._/_.'___.-`         (_______/   |__\|    \_______)                 ๐
+//
+//                  __   __  ___   __    __         __       ___         _______
+//                 |"  |/  \|  "| /" |  | "\       /""\     |"  |       /"     "|
+//      ๐          |'  /    \:  |(:  (__)  :)     /    \    ||  |      (: ______)
+//                 |: /'        | \/      \/     /' /\  \   |:  |   ₒ   \/    |
+//                  \//  /\'    | //  __  \\    //  __'  \   \  |___    // ___)_
+//                  /   /  \\   |(:  (  )  :)  /   /  \\  \ ( \_|:  \  (:      "|
+//                 |___/    \___| \__|  |__/  (___/    \___) \_______)  \_______)
+//                                                                                     ₒ৹
+//                          ___             __       _______     ________
+//         _               |"  |     ₒ     /""\     |   _  "\   /"       )
+//       ><_>              ||  |          /    \    (. |_)  :) (:   \___/
+//                         |:  |         /' /\  \   |:     \/   \___  \
+//                          \  |___     //  __'  \  (|  _  \\    __/  \\          \_____)\_____
+//                         ( \_|:  \   /   /  \\  \ |: |_)  :)  /" \   :)         /--v____ __`<
+//                          \_______) (___/    \___)(_______/  (_______/                  )/
+//                                                                                        '
+//
+//            ๐                          .    '    ,                                           ₒ
+//                         ₒ               _______
+//                                 ____  .`_|___|_`.  ____
+//                                        \ \   / /                        ₒ৹
+//                                          \ ' /                         ๐
+//   ₒ                                        \/
+//                                   ₒ     /      \       )                                 (
+//           (   ₒ৹               (                      (                                  )
+//            )                   )               _      )                )                (
+//           (        )          (       (      ><_>    (       (        (                  )
+//     )      )      (     (      )       )              )       )        )         )      (
+//    (      (        )     )    (       (              (       (        (         (        )
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@big-whale-labs/seal-hub-kit/contracts/SealHubChecker.sol";
+import "./NullifierCreatorVerifier.sol";
+
+contract SealHubMemorabiliaToken is ERC20, SealHubChecker {
+  // State
+  mapping(uint => bool) public nullifiers;
+  NullifierCreatorVerifier private nullifierCreatorVerifier;
+  SealHubChecker private sealHubChecker;
+
+  constructor(
+    address _nullifierCreatorVerifier,
+    address _sealHub
+  ) ERC20("SealHubMemorabiliaToken", "SHMT") SealHubChecker(_sealHub) {
+    nullifierCreatorVerifier = NullifierCreatorVerifier(
+      _nullifierCreatorVerifier
+    );
+  }
+
+  function mint(
+    uint[2] calldata _pA,
+    uint[2][2] calldata _pB,
+    uint[2] calldata _pC,
+    uint[2] calldata _pubSignals
+  ) public {
+    // Check the nullifier
+    require(!nullifiers[_pubSignals[1]], "Nullifier has already been used");
+    // Check the SealHub commitment
+    _checkSealHub(_pubSignals[0]);
+    // Check the ZK proof
+    require(
+      nullifierCreatorVerifier.verifyProof(_pA, _pB, _pC, _pubSignals),
+      "Invalid proof"
+    );
+    // Mint the token
+    _mint(msg.sender, 1);
+    // Add the nullifier
+    nullifiers[_pubSignals[1]] = true;
+  }
+}
